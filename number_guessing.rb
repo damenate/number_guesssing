649be87e857c5646ef4774dde69741cfdfbf0e86
Da-Me <damekim@DaMes-MacBook-Pro.local>
2015-09-01 16:30:41 -0400

    number game add

---
+++ number_guessing.rb
@@ -0,0 +1,35 @@
+def random_number()
+  number = rand(1..100)
+end
+
+previous_guesses = []
+correct_number = random_number
+counter = 0
+
+puts "Guess a number between 1 and 100 and you win! You have 5 chances!"
+puts "What is your guess?"
+
+loop do
+  guess = gets.chomp
+
+  if previous_guesses.include?(guess.to_i)
+    puts "I pity the fool who guesses the same thing TWICE! Guess again!"
+
+  elsif guess.to_i > correct_number.to_i
+    puts "I pity the fool who guesses too high! Guess again!"
+    previous_guesses << guess.to_i
+
+  elsif guess.to_i < correct_number.to_i
+    puts "I pity the fool who guesses too low! Guess again!"
+    previous_guesses << guess.to_i
+
+  elsif correct_number.to_i == guess.to_i
+    puts "WINNER WINNER CHICKEN DINNER!"
+    break
+  end
+     counter += 1
+  if counter == 5
+    puts "Thats 5 guesses sucker, YOU LOSE!"
+    break
+  end
+end
